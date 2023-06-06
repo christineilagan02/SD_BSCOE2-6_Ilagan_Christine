3acd604ab866107e3f68d7e13386595327561c59
Panda_Tine <christineilagan02@gmail.com>
2023-06-06 13:27:29 +0800

    Added user input functionality to find the maximum value in an array.

--- MaximumValue.java
+++ MaximumValue.java
@@ -1,8 +1,21 @@
 // 1. Finding the Maximum Value in an Array:
 
+import java.util.Scanner;
+
 public class MaximumValue {
     public static void main(String[] args) {
-        int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+
+        int[] numbers = new int[n];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+
         int max = numbers[0];
 
         for (int i = 1; i < numbers.length; i++) {
