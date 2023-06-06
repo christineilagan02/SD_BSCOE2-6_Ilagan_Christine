aa0cccf7dee17231a07adc2a1b64e2e959a7a77c
Panda_Tine <christineilagan02@gmail.com>
2023-06-06 15:08:33 +0800

    Added code to generate and print the Fibonacci series up to a specified number of terms.

---
+++ FibonacciSeries.java
@@ -0,0 +1,17 @@
+// 3. Fibonacci Series: (0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int n = 10;
+        int t1 = 0, t2 = 1;
+        System.out.print("First " + (n + 1) + " terms: ");
+
+        for (int i = 0; i <= n; i++) {
+            System.out.print(t1 + " ");
+
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+        }
+    }
+}
