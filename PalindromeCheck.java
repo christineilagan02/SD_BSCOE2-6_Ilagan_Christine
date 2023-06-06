10e4fe65ba0519b1fa02df95fee685eb61df17be
Panda_Tine <christineilagan02@gmail.com>
2023-06-06 17:24:27 +0800

    I enhance the code and this is the final code.

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,9 +1,14 @@
 // 5. Palindrome Check:
 // A palindrome is a word, phrase, number, or other sequence of characters that reads the same backward as forward.
 
+import java.util.Scanner;
+
 public class PalindromeCheck {
     public static void main(String[] args) {
-        String original = "racecar";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("\u001B[35m Enter a word: \u001B[0m");
+        String original = scanner.nextLine();
         String reversed = "";
 
         for (int i = original.length() - 1; i >= 0; i--) {
@@ -12,10 +17,22 @@ public class PalindromeCheck {
 
         boolean isPalindrome = original.equalsIgnoreCase(reversed);
 
+        System.out.println("\u001B[33m********************************************");
+        System.out.println("*             PALINDROME CHECK             *");
+        System.out.println("********************************************\u001B[0m");
+        System.out.println();
+
+        System.out.println("\u001B[36mOriginal word: \u001B[0m" + original);
+        System.out.println("\u001B[36mReversed word: \u001B[0m" + reversed);
+
         if (isPalindrome) {
-            System.out.println(original + " is a palindrome.");
+            System.out.println("\n\t\u001B[32m     It is a palindrome.\n\u001B[0m");
         } else {
-            System.out.println(original + " is not a palindrome.");
+            System.out.println("\n\t\u001B[31m  It is not a palindrome.\n\u001B[0m");
         }
+
+        System.out.println("\u001B[33m********************************************\u001B[0m");
+
+        scanner.close();
     }
 }
