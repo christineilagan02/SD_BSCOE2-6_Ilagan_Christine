377a46f38f4abc3055da7e4db3cbe41379da9bd2
Panda_Tine <christineilagan02@gmail.com>
2023-06-06 16:22:51 +0800

    Improved Fibonacci Series code with enhanced console output and colorful elements.

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -6,18 +6,13 @@ public class FibonacciSeries {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print();
-        System.out.print("\u001B[34m");
-        System.out.println("====== Fibonacci Series ======");
-        System.out.print("\u001B[0m"); 
-        System.out.print(); 
+        System.out.println("\n\u001B[34m========= Fibonacci Series =========\u001B[0m"); 
         
         System.out.print("Enter the number o terms: ");
         int n = scanner.nextInt();
         int t1 = 0, t2 = 1;
-        System.out.print("\u001B[32m");
-        System.out.print("First " + n + " terms: ");
-        System.out.print(">> \u001B[0m");
+        System.out.print("\n\u001B[32mFirst " + n + " terms: ");
+        System.out.print("\n>> \u001B[0m");
 
         for (int i = 0; i <= n; i++) {
             System.out.print(t1 + " ");
@@ -27,9 +22,7 @@ public class FibonacciSeries {
             t2 = sum;
         }
 
-        System.out.println("\u001B[34m\n=============================\u001B[0m");
-        System.out.print();
-
+        System.out.println("\u001B[34m\n====================================\u001B[0m\n");
         scanner.close();
     }
 }
