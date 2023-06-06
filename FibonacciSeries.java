354c517418293e5e16eb50cad8e589f0c50c2a09
Panda_Tine <christineilagan02@gmail.com>
2023-06-06 15:21:09 +0800

    Added code to generate and print the Fibonacci series up to a specified number of terms based on user input.

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,10 +1,15 @@
 // 3. Fibonacci Series: (0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55)
 
+import java.util.Scanner;
+
 public class FibonacciSeries {
     public static void main(String[] args) {
-        int n = 10;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number o terms: ");
+        int n = scanner.nextInt();
         int t1 = 0, t2 = 1;
-        System.out.print("First " + (n + 1) + " terms: ");
+        System.out.print("First " + n + " terms: ");
 
         for (int i = 0; i <= n; i++) {
             System.out.print(t1 + " ");
@@ -13,5 +18,7 @@ public class FibonacciSeries {
             t1 = t2;
             t2 = sum;
         }
+
+        scanner.close();
     }
 }
